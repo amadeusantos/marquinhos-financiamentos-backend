a158942915043ce70d00d462ef0807f983439932
amadeusantos <amadeusantos31@gmail.com>
2023-11-16 21:20:20 -0300

    fist commit

---
+++ src/main/java/com/marquinhos/financiamentos/backend/MarquinhosFinanciamentosApplication.java
@@ -0,0 +1,13 @@
+package com.marquinhos.financiamentos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarquinhosFinanciamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarquinhosFinanciamentosApplication.class, args);
+	}
+
+}
