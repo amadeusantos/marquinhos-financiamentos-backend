8ce9a446277b01313120411d5889cb7bb895194c
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2023-11-21 19:38:55 -0300

    feat: realizando tratamento de erros e padronizando código

--- src/main/java/com/marquinhos/financiamentos/backend/controller/FinancingController.java
+++ src/main/java/com/marquinhos/financiamentos/backend/controller/FinancingController.java
@@ -2,7 +2,6 @@ package com.marquinhos.financiamentos.backend.controller;
 
 import com.marquinhos.financiamentos.backend.service.FinancingService;
 import com.marquinhos.financiamentos.backend.util.exceptions.FinancingException;
-import com.marquinhos.financiamentos.backend.util.exceptions.VehicleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +19,11 @@ public class FinancingController {
   private FinancingService financingService;
 
   @GetMapping
-  public ResponseEntity<?> getFinancingValue(@RequestParam double amountFinanced, int numberInstallments){
-    try{
+  public ResponseEntity<?> getFinancingValue(@RequestParam double amountFinanced,
+      int numberInstallments) {
+    try {
       return ResponseEntity.ok(financingService.calcFinancing(amountFinanced, numberInstallments));
-    } catch(FinancingException e){
+    } catch (FinancingException e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
 
--- src/main/java/com/marquinhos/financiamentos/backend/service/FinancingService.java
+++ src/main/java/com/marquinhos/financiamentos/backend/service/FinancingService.java
@@ -1,23 +1,33 @@
 package com.marquinhos.financiamentos.backend.service;
 
 import com.marquinhos.financiamentos.backend.model.Financing;
+import com.marquinhos.financiamentos.backend.util.exceptions.FinancingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class FinancingService {
   private static final double MONTHLY_INTEREST =
       0.015; //1,5%
-  public Financing calcFinancing(double amountFinanced, int numberInstallments ) {
-    double installmentsValue = amountFinanced * MONTHLY_INTEREST / (1 - Math.pow(1 / (1 + MONTHLY_INTEREST), numberInstallments));
+
+  public Financing calcFinancing(double amountFinanced, int numberInstallments) {
+    if (!checkNumber(amountFinanced) || !checkNumber(numberInstallments)) {
+      throw new FinancingException("Numbers must be more than 0");
+    }
+
+    double installmentsValue = amountFinanced * MONTHLY_INTEREST / (1 - Math.pow(1
+        / (1 + MONTHLY_INTEREST), numberInstallments));
     double finalAmount = installmentsValue * numberInstallments;
 
-    Financing values = new Financing();
-    values.setFinalAmount(finalAmount);
-    values.setInstallmentsValue(installmentsValue);
+    Financing financingValues = new Financing();
+    financingValues.setFinalAmount(finalAmount);
+    financingValues.setInstallmentsValue(installmentsValue);
 
-    return values;
+    return financingValues;
+  }
+
+  public boolean checkNumber(double number) {
+    return !(number < 1);
   }
 }
