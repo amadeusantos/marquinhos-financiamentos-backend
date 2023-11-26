1c7756817ba0a4649cd56160f90af07de552b09d
amadeusantos <amadeusantos31@gmail.com>
2023-11-26 09:13:55 -0300

    [feat] create filter for models

--- src/main/java/com/marquinhos/financiamentos/backend/util/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/marquinhos/financiamentos/backend/util/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.marquinhos.financiamentos.backend.util.exceptions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,6 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(VehicleException.class)
     public ResponseEntity<ExceptionResponse> handleGenericVehicleException(VehicleException exception) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException() {
+        return ResponseEntity.badRequest().body(new ExceptionResponse("Accepted types are CARS, MOTORCYCLES and TRUCKS"));
     }
 
 }
